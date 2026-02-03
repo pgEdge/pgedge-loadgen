8cf84e27323516cbb48d411f939ee757ecece8f8
Dave Page <dpage@pgadmin.org>
2026-02-03 14:03:19 +0000

    Copyright notice updates

--- internal/apps/ecommerce/app.go
+++ internal/apps/ecommerce/app.go
@@ -2,7 +2,7 @@
 //
 // pgEdge Load Generator
 //
-// Portions copyright (c) 2025 - 2026, pgEdge, Inc.
+// Copyright (c) 2025 - 2026, pgEdge, Inc.
 // This software is released under The PostgreSQL License
 //
 //-------------------------------------------------------------------------
